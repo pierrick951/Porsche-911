1484500c17636d6eafb2b77268969cdc7060a923
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-11-11 15:06:12 -0500

    update

--- src/app/_layout/Stats.tsx
+++ src/app/_layout/Stats.tsx
@@ -1,10 +1,14 @@
+import { motion, animate, useInView } from "framer-motion";
 import Image from "next/image";
 import back from "@/public/back.avif";
 import { dataStats } from "../data";
 import Button from "../_components/Button";
 import { phraseStat } from "../data";
+import { useRef } from "react";
 
 function Stats() {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: true });
   return (
     <div className="h-auto bg-zinc-950 flex  flex-col xl:flex-row lg:gap-10 justify-center items-center py-16">
       <div>
@@ -12,16 +16,29 @@ function Stats() {
           return (
             <div key={item.id}>
               <div className=" flex flex-col justify-center xl:text-start items-center xl:items-start px-6">
-                <div className=" flex flex-row gap-2 py-3 lg:py-5 items-end bg-red-4 text-nowrap ">
-                  <h2 className="text-white text-4xl  md:text-8xl">
+                <div
+                  ref={containerRef}
+                  className=" flex flex-row gap-2 py-3 lg:py-5 items-end bg-red-4 text-nowrap "
+                >
+                  <motion.h2
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 0.5 }}
+                    className="text-white text-4xl  md:text-8xl"
+                  >
                     {item.title}
-                  </h2>
+                  </motion.h2>
                   <small className="text-white font-semibold text-2xl">
                     {item.sub}
                   </small>
-                  <h2 className="text-white text-4xl  md:text-8xl">
+                  <motion.h2
+                    className="text-white text-4xl  md:text-8xl"
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 0.5 }}
+                  >
                     <span>{item.title2}</span>
-                  </h2>
+                  </motion.h2>
                   <small className="text-white font-semibold text-xl">
                     {item.sub2}
                   </small>
@@ -37,7 +54,12 @@ function Stats() {
           <Button text={phraseStat} />
         </div>
       </div>
-      <Image className=" lg:max-w-2xl" src={back} alt="back of the porsche" loading="lazy" />
+      <Image
+        className=" lg:max-w-2xl"
+        src={back}
+        alt="back of the porsche"
+        loading="lazy"
+      />
     </div>
   );
 }
